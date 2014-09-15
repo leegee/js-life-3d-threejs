4f9594410f4472071457eaf619e982d8e65dc193
Lee Goddard <lee@leegoddard.net>
2014-09-15 01:22:24 +0100

    Faster layout

--- app/main.js
+++ app/main.js
@@ -1,9 +1,18 @@
 define(["Life", "three"], function main (Life, THREE) {
     var game = new Life({
-        matrix: {
-            x: 9,
-            y: 9,
-            z: 3
-        }
+        fullscreen          : true,
+        screenWidth         : window.innerWidth,
+        screenHeight        : window.innerHeight,
+        frustumVerticalFov  : 40,
+        frustumNearPlane    : .01,
+        frustumFarPlane     : 20000,
+        padFactor           : {x: 2.5, y: 2.5, z: 2.5},
+        minFrameDuration    : 1000,
+        // matrix              : { x: 12, y: 12, z: 1 },
+        map : [
+            [ 0, 1, 0],
+            [ 0, 1, 0],
+            [ 0, 1, 0],
+        ]
     });
 });
