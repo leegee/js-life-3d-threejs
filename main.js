98e8f5a0f8af0b7df08cf2951b5d09a9600cbb30
Lee Goddard <lee@leegoddard.net>
2014-09-15 17:06:22 +0100

    stop when no change

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 define(["Life", "three"], function main (Life, THREE) {
+
+    var maps = {};
+    maps.simple_osc1 = [
+        [ 0, 0, 0 ],
+        [ 0, 1, 0 ],
+        [ 0, 1, 0 ],
+        [ 0, 1, 0 ],
+        [ 0, 0, 0 ]
+    ];
+    maps.simple_osc2 = [
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 1, 1, 1, 0 ],
+        [ 0, 1, 1, 1, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0 ]
+    ];
+    maps.glider = [
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0 ],
+        [ 0, 1, 0, 0, 0, 0 ],
+        [ 0, 1, 1, 0, 0, 0 ],
+        [ 1, 0, 1, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0 ]
+    ];
+
     var game = new Life({
         fullscreen          : true,
         screenWidth         : window.innerWidth,
@@ -7,14 +34,8 @@ define(["Life", "three"], function main (Life, THREE) {
         frustumNearPlane    : .01,
         frustumFarPlane     : 20000,
         padFactor           : {x: 2.5, y: 2.5, z: 2.5},
-        minFrameDuration    : 1000,
+        minFrameDuration    : 100,
         // matrix              : { x: 12, y: 12, z: 1 },
-        map : [
-            [ 0, 0, 0 ],
-            [ 0, 1, 0 ],
-            [ 0, 1, 0 ],
-            [ 0, 1, 0 ],
-            [ 0, 0, 0 ]
-        ]
+        map : maps.glider
     });
 });
